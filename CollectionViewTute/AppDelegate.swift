3442f1695482bd2e3027d4ef4c9164baf8e4b9f6
CeceXX <cesare.decal@gmail.com>
2015-05-05 15:46:42 +0200

    Update AppDelegate.swift

--- CollectionViewTute/AppDelegate.swift
+++ CollectionViewTute/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  CollectionViewTutorial
+//  CollectionViewTute
 
 import UIKit
 
@@ -8,7 +8,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -36,7 +35,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
